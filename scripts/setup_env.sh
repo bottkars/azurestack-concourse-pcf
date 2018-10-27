c263db66a53e8a9a1ab8ee5db5adbf004198af1e
bottk <bottk@bottks-MacBook-Pro.local>
2018-10-27 09:49:06 +0200

    libreadline fix

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -56,7 +56,7 @@ home_dir="/home/$username"
 
 # https://bosh.io/docs/cli-v2-install/#additional-dependencies
 echo "Installing OS specified dependencies for bosh create-env command"
-retryop "apt-get update && apt-get install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3"
+retryop "apt-get update && apt-get install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline7 libreadline-dev libyaml-dev libsqlite3-dev sqlite3"
 
 echo "Installing BOSH CLI"
 bosh_cli_url=$(get_setting BOSH_CLI_URL)
